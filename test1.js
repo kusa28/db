d9e37c247b65d65463a8f23ca4619c786647d55d
kusa28 <123868258+kusa28@users.noreply.github.com>
2023-03-09 13:24:18 +0900

    Update test1.js

--- test1.js
+++ test1.js
@@ -22,8 +22,8 @@ navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
 
         const db = 20 * Math.log10(volume / 0.00002);
         document.getElementById("volume-value").textContent = volume.toFixed(2);
-        if (volume-value > 100) {
-            document.getElementById("volume-value").textContent = "こんにちは";
+        if (volume.toFixed(2) > 100) {
+            document.getElementById("hyouzi").textContent = "こんにちは";
 }
     }
 
